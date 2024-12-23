06e683f6cd8f224754ae7c0bfbced4f7a9c214d0
Fabricio Robles <fabrobles92@gmail.com>
2024-12-22 18:35:34 -0600

    feat: update user and picture using hardcoded data

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,14 +1,49 @@
 'use client';
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '@/layouts/Header';
 import Sidebar from '@/layouts/profile/Sidebar';
 import ProfilePicture from '@/components/profile/ProfilePicture';
 import ProfileForm from '@/components/profile/ProfileForm';
 import AccountOverview from '@/components/profile/AccountOverview';
 import { useTranslation } from 'react-i18next';
+import {
+  UPDATE_PROFILE_IMAGE,
+  UPDATE_USER_RECORD,
+} from '@/graphql/queries/userMutations.graphql';
+import { useMutation } from '@apollo/client';
 
 const Home: React.FC = () => {
   const { t } = useTranslation();
+  const [user, setUser] = useState(null);
+
+  // GraphQL mutations
+  const [updateUser] = useMutation(UPDATE_USER_RECORD);
+  const [updateProfileImage] = useMutation(UPDATE_PROFILE_IMAGE);
+
+  const handleSave = async () => {
+    try {
+      const input = {
+        first_name: 'John',
+        last_name: 'Doe',
+        summary: 'Updated summary',
+        phone_number: '88588289',
+        country_code: '+506',
+        location: 'New York',
+      };
+      const userId = '97633789-d1ac-4526-80dc-d0a92ff3502e';
+
+      await updateUser({
+        variables: { userId, input },
+      });
+
+      const imageUrl = 'https://placecats.com/millie/300/150';
+      const imageKey = 'cat';
+      await updateProfileImage({ variables: { userId, imageUrl, imageKey } });
+    } catch (error) {
+      console.error('Error updating profile:', error);
+    }
+  };
+
   return (
     <div className="bg-gray-50 min-h-screen flex flex-col dark:bg-dark-background">
       <Header />
@@ -30,7 +65,10 @@ const Home: React.FC = () => {
           <ProfileForm />
           <AccountOverview />
           <div className="text-right mt-6">
-            <button className="bg-orange-500 text-white px-4 py-2 rounded-lg hover:bg-orange-600 transition-colors">
+            <button
+              className="bg-orange-500 text-white px-4 py-2 rounded-lg hover:bg-orange-600 transition-colors"
+              onClick={handleSave}
+            >
               {t('profile.overView.saveBtn')}
             </button>
           </div>
