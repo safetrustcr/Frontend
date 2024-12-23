c19a01a3857e16184e91a3abd70b77904af594e8
Fabricio Robles <fabrobles92@gmail.com>
2024-12-22 21:58:56 -0600

    feat: Introduce state in the Profile Form

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,5 +1,4 @@
 'use client';
-import React, { useState } from 'react';
 import Header from '@/layouts/Header';
 import Sidebar from '@/layouts/profile/Sidebar';
 import ProfilePicture from '@/components/profile/ProfilePicture';
@@ -10,11 +9,31 @@ import {
   UPDATE_PROFILE_IMAGE,
   UPDATE_USER_RECORD,
 } from '@/graphql/queries/userMutations.graphql';
-import { useMutation } from '@apollo/client';
+import { ApolloError, useMutation } from '@apollo/client';
+import { ChangeEvent, useState } from 'react';
+import { toast } from 'react-toastify';
 
 const Home: React.FC = () => {
+  const [formData, setFormData] = useState({
+    first_name: 'Randall',
+    last_name: 'Valenciano',
+    summary:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+    phone_number: '6498 6325',
+    country_code: '+506',
+    location: 'Costa Rica, San José',
+  });
+
+  const [picture, setPicture] = useState('img/person.png');
+
+  const handleChange = (
+    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>
+  ) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  };
+
   const { t } = useTranslation();
-  const [user, setUser] = useState(null);
 
   // GraphQL mutations
   const [updateUser] = useMutation(UPDATE_USER_RECORD);
@@ -23,14 +42,14 @@ const Home: React.FC = () => {
   const handleSave = async () => {
     try {
       const input = {
-        first_name: 'John',
-        last_name: 'Doe',
-        summary: 'Updated summary',
-        phone_number: '88588289',
-        country_code: '+506',
-        location: 'New York',
+        first_name: formData.first_name,
+        last_name: formData.last_name,
+        summary: formData.summary,
+        phone_number: formData.phone_number,
+        country_code: formData.country_code,
+        location: formData.location,
       };
-      const userId = '97633789-d1ac-4526-80dc-d0a92ff3502e';
+      const userId = '97633789-d1ac-4526-80dc-d0a92ff3502e'; // Obtained from User table
 
       await updateUser({
         variables: { userId, input },
@@ -38,9 +57,34 @@ const Home: React.FC = () => {
 
       const imageUrl = 'https://placecats.com/millie/300/150';
       const imageKey = 'cat';
+
       await updateProfileImage({ variables: { userId, imageUrl, imageKey } });
+      setPicture(imageUrl);
+      toast.success('User profile updated successfully', {
+        position: 'top-center',
+      });
     } catch (error) {
-      console.error('Error updating profile:', error);
+      if (error instanceof ApolloError) {
+        console.error('ApolloError:', error);
+        if (error.graphQLErrors?.length) {
+          error.graphQLErrors.forEach((graphQLError) => {
+            let message = graphQLError.message;
+            if (message.includes('valid_phone_format')) {
+              toast.error('Invalid phone format', { position: 'top-center' });
+            } else if (message.includes('valid_country_code')) {
+              toast.error('Invalid country code format', {
+                position: 'top-center',
+              });
+            } else {
+              toast.error(`Unhandled error: ${message}`);
+            }
+          });
+        } else {
+          console.error('Unknown GraphQL error:', error.message);
+        }
+      } else {
+        console.error('Unexpected error:', error);
+      }
     }
   };
 
@@ -54,15 +98,24 @@ const Home: React.FC = () => {
             {t('profile.title')}
           </h1>
           <div className="flex flex-col lg:flex-row items-center gap-6 mb-6">
-            <ProfilePicture />
+            <ProfilePicture src={picture} />
             <textarea
+              name="summary"
               placeholder={t('profile.summaryHolder')}
-              defaultValue="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."
+              value={formData.summary}
               className="border border-gray-300 dark:border-gray-700 rounded-lg p-4 w-full text-gray-900 dark:text-dark-primary bg-white dark:bg-dark-surface placeholder-gray-500 dark:placeholder-gray-400"
+              onChange={handleChange}
               rows={3}
             />
           </div>
-          <ProfileForm />
+          <ProfileForm
+            first_name={formData.first_name}
+            last_name={formData.last_name}
+            phone_number={formData.phone_number}
+            country_code={formData.country_code}
+            location={formData.location}
+            onInputChange={handleChange}
+          />
           <AccountOverview />
           <div className="text-right mt-6">
             <button
