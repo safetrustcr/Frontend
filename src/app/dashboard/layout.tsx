e9a89599d6295b47c3b22bab9216e428b34d8f44
MPSxDev <mparedes.digital@gmail.com>
2025-02-25 13:51:00 -0300

    feat: Add Sidebar and Header to dashboard layout

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -2,6 +2,8 @@
 
 import { redirect } from "next/navigation";
 import { useGlobalAuthenticationStore } from "@/core/store/data";
+import { SideBar } from "@/components/layouts/SideBar";
+import { Header } from "@/components/layouts/Header";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   const address = useGlobalAuthenticationStore((state) => state.address);
@@ -11,7 +13,15 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
     redirect("/");
   }
 
-  return <div>{children}</div>;
+  return (
+    <div className="flex h-full">
+      <Header />
+      <SideBar notificationCount={1} />
+      <main className="flex-1 p-2 pt-16 md:p-10 md:ml-48">
+        {children}
+      </main>
+    </div>
+  );
 };
 
 export default Layout;
